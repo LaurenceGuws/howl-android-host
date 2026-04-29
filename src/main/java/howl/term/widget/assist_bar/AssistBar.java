dadcb427b747bdf4eff0c6f5402a2b07201c4a12
Laurence <lggousie46@gmail.com>
2026-04-29 13:54:52 +0200

    cleanup unused field

--- src/main/java/howl/term/widget/assist_bar/AssistBar.java
+++ src/main/java/howl/term/widget/assist_bar/AssistBar.java
@@ -8,13 +8,11 @@ public final class AssistBar {
     private final WindowRuntime winRt;
     private final InputRuntime inputRt;
     private final android.widget.FrameLayout view;
-    private boolean shown;
 
     public AssistBar(android.app.Activity activity, WindowRuntime winRt) {
         this.winRt = winRt;
         this.inputRt = new InputRuntime();
         this.view = winRt.container(activity);
-        this.shown = true;
         winRt.setBackground(view, 0xFF1A1A1A);
         view.setClickable(true);
         view.setFocusable(true);
@@ -28,12 +26,10 @@ public final class AssistBar {
     }
 
     public void show() {
-        shown = true;
         winRt.setVisible(view);
     }
 
     public void hide() {
-        shown = false;
         winRt.setGone(view);
     }
 }
